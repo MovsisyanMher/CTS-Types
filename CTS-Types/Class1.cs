938524857a9589a29136241bed4dbf7ccbef383b
MovsisyanMher <mher.movsisyan2011@yandex.ru>
2018-12-20 14:21:40 -0800

    DevPush 1

--- CTS-Types/Class1.cs
+++ CTS-Types/Class1.cs
@@ -1,12 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CTS_Types
+﻿namespace CTS_Types
 {
-    public class Class1
+    public abstract class Person
     {
+        public string Name { get; set; }
+
+        public System.Guid Id { get; }
+
+        public Person(string Name)
+        {
+            this.Name = Name;
+            Id = System.Guid.NewGuid();
+        }
+        
+    }
+
+    public class Employee : Person
+    {
+        public Employee(string Name) : base(Name){}
+
+        public Employee(string Name, int Salary) : base(Name)
+        {
+            this.Salary = Salary;
+        }
+
+        /// <summary>
+        /// Salary in USD cents
+        /// </summary>
+        public int Salary { get; set; }
+
+
+        public override bool Equals(object obj)
+        {
+            try
+            {
+                var person = obj as Person;
+                if (person.Id == Id) return true;
+            }
+            catch (System.Exception)
+            {
+                return false;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.ToString().GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    public sealed class Programmer : Employee
+    {
+        public Programmer(string Name) : base(Name)
+        {
+
+        }
+
+        public Programmer(string Name, int Salary) : base(Name, Salary)
+        {
+
+        }
+
+
     }
 }
